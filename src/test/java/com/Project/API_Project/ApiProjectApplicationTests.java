125f6093f965d94c38793c5ee4902f1d5f8a4f2e
Leonardo Meirelles <leo.dino@hotmail.com>
2026-09-11 12:22:03 -0300

    feat: initial commit - estrutura base da API com Spring Boot e Docker

---
+++ src/test/java/com/Project/API_Project/ApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project.API_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
